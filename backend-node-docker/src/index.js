430919950525a3c46c79ff33cbdbadf8ed73819f
Angel-DGP <angel.patinogd@gmail.com>
2025-09-29 22:34:41 -0500

    Reto 4 realizado

--- backend-node-docker/src/index.js
+++ backend-node-docker/src/index.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT;
+const jwlSecret = process.env.JWT_SECRET
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('API BACKEND WORKED!');
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+console.log('JWT Secret:', jwlSecret);
+console.log('Port:', port)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
